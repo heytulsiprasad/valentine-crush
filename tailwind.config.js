7c43af23c40b67e2738b86c4554bfa92fea4fcfa
Tulsi Prasad <tulsi.prasad50@gmail.com>
2024-02-08 11:07:42 +0530

    Add desktop first screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xlmax: { max: "90rem" },
+        lgmax: { max: "68rem" },
+        mdmax: { max: "45rem" },
+        smmax: { max: "35rem" },
+      },
+    },
   },
   plugins: [],
 };
